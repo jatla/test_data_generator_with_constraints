ba0704bc01bc8b11427c9fa5c2cef940be982f11
JayaPrakash Reddy Atla <jatla@Sunithas-MacBook-Air.local>
2014-03-11 21:34:06 -0700

    Adding rspec tests

--- spec/test_data_generator_with_constraints_spec.rb
+++ spec/test_data_generator_with_constraints_spec.rb
@@ -1,7 +1,37 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TestDataGeneratorWithConstraints" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+	context "#generateTestDataForModel" do
+  		it "successfully generates pairwise data for simple model" do
+
+  			dataModel =
+  				{
+  					model:
+  					{
+  						supply: (0..3),
+						demand: (0..3)
+  					},
+  					constraints: {}
+				}
+    		pairWiseData  = TestDataGeneratorWithConstraints.instance.generateTestDataForModel(dataModel[:model])
+    		pairWiseData.length.should eq 16
+    	end
+    	it "successfully generates pairwise data for simple models with constraints" do
+    		constraint = lambda { |t| t if (t[:supply] >= t[:demand])}
+  			dataModel =
+  				{
+  					model:
+  					{
+  						supply: (0..3),
+						demand: (0..3)
+  					},
+					constraints:
+					{
+						model: [constraint]
+					}
+				}
+    		pairWiseData  = TestDataGeneratorWithConstraints.instance.generate(dataModel)
+    		pairWiseData.length.should eq 10
+    	end
+	end
 end

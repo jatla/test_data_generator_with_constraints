af0c0d827b4736c4c17c247555148709484d7525
JayaPrakash Reddy Atla <jatla@Sunithas-MacBook-Air.local>
2014-03-13 13:48:45 -0700

    Updated README & rspec tests.

--- bin/test_data_generator_with_constraints.rb
+++ bin/test_data_generator_with_constraints.rb
@@ -6,27 +6,27 @@ options = {:m => nil, :o => nil}
 
 parser = OptionParser.new do |opts|
 
-	opts.banner = "Usage: test_data_generator_with_constraints.rb [options] "
+    opts.banner = "Usage: test_data_generator_with_constraints.rb [options] "
 
-	opts.on('-m filePath', '--modelFilePath filePath', 'Path to file containing model and constaints. This is a required parameter.') do |filePath|
-		options[:m] = filePath
-	end
+    opts.on('-m filePath', '--modelFilePath filePath', 'Path to file containing model and constaints. This is a required parameter.') do |filePath|
+        options[:m] = filePath
+    end
 
-	opts.on('-o filePath', '--outputFilePath filePath', 'Output file where generated test data set is written to. If not provided test data set would be generated to temp.txt') do |filePath|
-		options[:o] = filePath
-	end
+    opts.on('-o filePath', '--outputFilePath filePath', 'Output file where generated test data set is written to. If not provided test data set would be generated to temp.txt') do |filePath|
+        options[:o] = filePath
+    end
 
-  	opts.on( '-h', '--help', 'Display this screen' ) do
-    	puts opts
-    	exit
-  	end
+    opts.on( '-h', '--help', 'Display this screen' ) do
+        puts opts
+        exit
+      end
 end
 
 parser.parse!
 
 if options[:m].nil?
-	puts parser
-	raise OptionParser::MissingArgument
+    puts parser
+    raise OptionParser::MissingArgument
 end
 
 load "#{options[:m]}"
--- spec/test_data_generator_with_constraints_spec.rb
+++ spec/test_data_generator_with_constraints_spec.rb
@@ -33,5 +33,70 @@ describe "TestDataGeneratorWithConstraints" do
     		pairWiseData  = TestDataGeneratorWithConstraints.instance.generate(dataModel)
     		pairWiseData.length.should eq 10
     	end
+      it "successfully generates pairwise data for complex models with constraints" do
+        complexConstraint = lambda {|t| t if ((t[:inner4] != t[:inner5]) && (t[:innermost2] != t[:innermost3]) && (t[:innermost1] > 3))}
+        dataModel =
+        {
+            model:
+            {
+                outer1:
+                {
+                    inner1:
+                    {
+                        innermost1: (1..5),
+                        innermost2: [true, false],
+                        innermost3: [true, false]
+                    },
+                    inner2: [1,-1]
+                },
+                outer2:
+                {
+                    inner3: (1..3),
+                    inner4: [true, false],
+                    inner5: [true, false]
+                }
+            },
+            constraints:
+            {
+                model: [complexConstraint]
+            }
+        }
+        pairWiseData  = TestDataGeneratorWithConstraints.instance.generate(dataModel)
+        pairWiseData.length.should eq 32
+      end
+      it "successfully generates pairwise data for complex models with multiple constraints" do
+        outerConstraint = lambda {|t| t if (t[:inner4] != t[:inner5])}
+        innerConstraint1 = lambda {|t| t if (t[:innermost2] != t[:innermost3])}
+        innerConstraint2 = lambda {|t| t if (t[:innermost1] > 3)}
+        dataModel =
+        {
+            model:
+            {
+                outer1:
+                {
+                    inner1:
+                    {
+                        innermost1: (1..5),
+                        innermost2: [true, false],
+                        innermost3: [true, false]
+                    },
+                    inner2: [1,-1]
+                },
+                outer2:
+                {
+                    inner3: (1..3),
+                    inner4: [true, false],
+                    inner5: [true, false]
+                }
+            },
+            constraints:
+            {
+                inner1: [innerConstraint1, innerConstraint2],
+                outer2: [outerConstraint]
+            }
+        }
+        pairWiseData  = TestDataGeneratorWithConstraints.instance.generate(dataModel)
+        pairWiseData.length.should eq 32
+      end
 	end
 end
